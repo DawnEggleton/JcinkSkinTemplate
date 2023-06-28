f64673a69bf16bd76c76f86e3638012b6814d323
DawnEggleton <dawn.eggleton@carleton.ca>
2023-06-28 08:12:41 -0300

    reset from vv push

--- public/templates/header.js
+++ public/templates/header.js
@@ -2,9 +2,6 @@
 Put your header HTML between the backticks below and it will auto-populate on all pages of this local copy.
 */
 
-const headerHTML = `
-<a href="index.html"><h1>Vae Victis</h1></a>
-<p><b>ve ˈvik.tis</b><br><i>latin &mdash;</i> woe to the vanquished</p>
-`;
+const headerHTML = ``;
 
 document.querySelector('body > header').innerHTML = headerHTML;
